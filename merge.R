a1c978c756f0b984048c035c26f83af393dbb3bd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:46:04 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,5 +1,6 @@
 
 library("ggplot2")
+library("ggmap")
 arrests <- USArrests
 arrests
 CombinedData <- merge(data,arrests,by.x = "statename",by.y = 0) #merged the columns of both the data frames by statename
