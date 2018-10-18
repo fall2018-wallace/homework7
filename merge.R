1ef0f6111f3fe87fa2e3db122d47ba89b2b91a0f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:46:56 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -4,6 +4,8 @@ library("ggmap")
 arrests <- USArrests
 arrests
 
-##1)	Read in the census dataset and the USArrests and merge them (just like HW6)
+#1)	Read in the census dataset and the USArrests and merge them (just like HW6)
 CombinedData <- merge(data,arrests,by.x = "statename",by.y = 0) #merged the columns of both the data frames by statename
 CombinedData
+
+#
