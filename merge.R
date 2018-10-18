41686c03a074bd0e5faff7d612134c7c8bc9c12f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:46:45 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -3,5 +3,7 @@ library("ggplot2")
 library("ggmap")
 arrests <- USArrests
 arrests
+
+##1)	Read in the census dataset and the USArrests and merge them (just like HW6)
 CombinedData <- merge(data,arrests,by.x = "statename",by.y = 0) #merged the columns of both the data frames by statename
 CombinedData
